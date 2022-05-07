5dcded5cf2de7e665399c3241ed11871815247bb
gonza7795 <gonza_7795@yahoo.com.ar>
2022-05-07 15:35:55 -0300

    hasta min 1.00

--- prueba-angular-logueo/src/app/guards/auth.guard.ts
+++ prueba-angular-logueo/src/app/guards/auth.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
@@ -8,24 +7,22 @@ import { AuthService } from '../services/auth.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
   constructor(
-    private authService: AuthService
-    
+    private authService: AuthService,
+    private router: Router
   ) {  }
   canActivate():boolean{
 
     if(!this.authService.isAuth()){
       console.log('Token no es válido o ya expiró');
-    //  this.router.navigate(['login']);
+      this.router.navigate(['login']);
       return false;
     }
     return true;
-
-     
-    }
-    
   }
   
+}
 
 
 
